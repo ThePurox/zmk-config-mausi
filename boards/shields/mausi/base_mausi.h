1dea3d51b14a9abdbe03ad450cb765bb6890b2bc
Nex Stuhlmüller <n+git@ncxst.de>
2026-04-09 10:04:26 +0200

    adjusted combos

--- boards/shields/mausi/base_mausi.h
+++ boards/shields/mausi/base_mausi.h
@@ -28,101 +28,101 @@
 /* usually on base layer */
         combo_q {
             timeout-ms = <40>;
-            key-positions = <1 2>;
+            key-positions = <2 3>;
             bindings = <&kp Q>;
             layers = <APT>;
         };
         combo_z {
             timeout-ms = <40>;
-            key-positions = <18 19>;
+            key-positions = <20 21>;
             bindings = <&kp DE_Z>;
         };
         combo_v {
             timeout-ms = <40>;
-            key-positions = <0 1>;
+            key-positions = <1 2>;
             bindings = <&kp V>;
             layers = <APT>;
         };
         combo_b {
             timeout-ms = <40>;
-            key-positions = <2 10>;
+            key-positions = <12 3>;
             bindings = <&kp B>;
             layers = <APT>;
         };
         combo_j {
           timeout-ms = <40>;
-          key-positions = <5 15>;
+          key-positions = <6 17>;
           bindings = <&kp J>;
           layers = <APT>;
         };
         combo_k {
           timeout-ms = <40>;
-          key-positions = <10 11>;
+          key-positions = <12 13>;
           bindings = <&kp K>;
           layers = <APT>;
         };
         combo_x {
           timeout-ms = <40>;
-          key-positions = <14 15>;
+          key-positions = <16 17>;
           bindings = <&kp X>;
           layers = <APT>;
         };
         combo_slash {
             timeout-ms = <40>;
-            key-positions = <22 23>;
+            key-positions = <24 25>;
             bindings = <&kp SLASH>;
         };
         combo_ent {
             timeout-ms = <40>;
-            key-positions = <21 22>;
+            key-positions = <23 24>;
             bindings = <&kp ENTER>;
         };
 /* caps */
         combo_caps-word {
             timeout-ms = <40>;
-            key-positions = <11 14>;
+            key-positions = <13 16>;
             bindings = <&caps_word>;
         };
         combo_capslock {
             timeout-ms = <40>;
-            key-positions = <0 7>;
+            key-positions = <0 9>;
             bindings = <&kp CAPSLOCK>;
         };
         /* Umlaute */
         combo_ue {
           timeout-ms = <20>;
-          key-positions = <7 26>;
+          key-positions = <8 28>;
           bindings = <&kp DE_UE>;
         };
         combo_oe {
           timeout-ms = <20>;
-          key-positions = <17 26>;
+          key-positions = <19 28>;
           bindings = <&kp DE_OE>;
         };
         combo_ae {
           timeout-ms = <20>;
-          key-positions = <15 26>;
+          key-positions = <17 28>;
           bindings = <&kp DE_AE>;
         };
         combo_sz {
           timeout-ms = <20>;
-          key-positions = <9 26>;
+          key-positions = <11 26>;
           bindings = <&kp DE_SZ>;
         };
         /* deletion */
         combo_bspc {
           timeout-ms = <40>;
-          key-positions = <5 6>;
+          key-positions = <7 6>;
             bindings = <&kp BSPC>;
         };
         combo_del {
             timeout-ms = <40>;
-            key-positions = <6 7>;
+            key-positions = <8 7>;
             bindings = <&kp DEL>;
         };
         combo_delword {
             timeout-ms = <40>;
-            key-positions = <5 7>;
+            key-positions = <6 8>;
             bindings = <&kp LC(BSPC)>;
         };
 /*F keys*/
@@ -156,7 +156,7 @@
             // global-quick-tap;
             bindings = <&kp>, <&kp>;
             // opposite side hand keys
-            hold-trigger-key-positions = <4 5 6 7 13 14 15 16 17 21 22 23 24 25 26 27>;
+            hold-trigger-key-positions = <5 6 7 8 9 15 16 17 18 19 23 24 25 26 27 28 29>;
         };
         hr: homerow_mods_right {
             compatible = "zmk,behavior-hold-tap";
@@ -169,7 +169,7 @@
             // global-quick-tap;
             bindings = <&kp>, <&kp>;
             // opposite side hand keys
-            hold-trigger-key-positions = <0 1 2 3 8 9 10 11 12 18 19 20 24 25 26 27>;
+            hold-trigger-key-positions = <0 1 2 3 4 10 11 12 13 14 20 21 22 26 27 28 29>;
         };
 
         my_lt: my_layer_taps {
@@ -183,22 +183,8 @@
             // global-quick-tap;
             bindings = <&mo &kp>, <&kp>;
             // non-thumb keys
-            hold-trigger-key-positions = <0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27>;
+            hold-trigger-key-positions = <0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29>;
         };
 
-        dotcol: dot_colon {
-            compatible = "zmk,behavior-mod-morph";
-            label = "DOT_COL";
-            #binding-cells = <0>;
-            bindings = <&kp DOT>, <&kp COLON>;
-            mods = <(MOD_LSFT|MOD_RSFT)>;
-        };
-        comsem: comma_semicolon {
-            compatible = "zmk,behavior-mod-morph";
-            label = "COM_SEM";
-            #binding-cells = <0>;
-            bindings = <&kp COMMA>, <&kp SEMI>;
-            mods = <(MOD_LSFT|MOD_RSFT)>;
-        };
     };
 };
